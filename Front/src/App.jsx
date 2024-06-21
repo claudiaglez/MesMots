852b4912f851dcd5b9d4f9659172e19944e35e30
Claudia González <claudia.glezg@gmail.com>
2024-06-21 17:51:19 +0200

    Install and configure with a button test shadcn

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import { Button } from './components/ui/Button'
 
 function App() {
 
   return (
     <>
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+     <Button>Hello</Button>
+    </div>
     </>
   )
 }
