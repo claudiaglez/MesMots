2fc0e1b140affe9a6fd80da011e87d2ac70e7dec
Claudia González <claudia.glezg@gmail.com>
2024-06-27 12:08:17 +0200

    Add basic structure to the ProfileForm

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import { ProfileForm } from './app/ui/ProfileForm'
 import { Button } from './components/ui/Button'
-
 function App() {
 
   return (
     <>
     <div>
      <Button>Hello</Button>
+     <ProfileForm></ProfileForm>
     </div>
     </>
   )
