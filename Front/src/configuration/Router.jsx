6ba7c02641df0a1b5ebeac38399f6933b2b7785f
Claudia González <claudia.glezg@gmail.com>
2024-09-04 13:35:29 +0200

    Add Form View to Routes

--- Front/src/configuration/Router.jsx
+++ Front/src/configuration/Router.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import App from '../App';
+import FormView from '@/components/views/FormView';
 
 const Router = () => {
   return (
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/ajouter" element={<FormView />} />
     </Routes>
     </BrowserRouter>
   )
