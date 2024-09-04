d8a0a5b1120e7cc4ab4cd87517267be4827e9354
Claudia González <claudia.glezg@gmail.com>
2024-09-04 14:08:06 +0200

    Delete FormView from the home page

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import { Button } from './components/ui/Button'
-import FormView from './components/views/FormView'
 function App() {
 
   return (
     <>
     <div>
      <Button>Hello</Button>
-     <FormView />
     </div>
     </>
   )
