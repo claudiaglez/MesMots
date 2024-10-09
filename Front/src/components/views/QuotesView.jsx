867a50c33933c088fa2b44dd24d5b919605621b4
Claudia González <claudia.glezg@gmail.com>
2024-10-09 16:58:33 +0200

    Add the edit form and center the save button

--- Front/src/components/views/QuotesView.jsx
+++ Front/src/components/views/QuotesView.jsx
@@ -1,222 +1,301 @@
-import Navbar from '@/app/ui/Navbar';
-import React, { useEffect, useState } from 'react';
-import { Card, CardContent, CardFooter } from '../ui/Card';
+import Navbar from "@/app/ui/Navbar";
+import React, { useEffect, useState } from "react";
+import { Card, CardContent, CardFooter } from "../ui/Card";
 import { FaTrash, FaPenNib, FaSave } from "react-icons/fa";
 import { IoCloseOutline } from "react-icons/io5";
-import axios from 'axios';
-import { Pagination } from '../../app/ui/pagination';
+import axios from "axios";
+import { Pagination } from "../../app/ui/pagination";
 
 const QuotesView = () => {
-    const [quotes, setQuotes] = useState([]);
-    const [selectedQuote, setSelectedQuote] = useState(null);
-    const [selectedColor, setSelectedColor] = useState('');
-    const [isEditing, setIsEditing] = useState(false);
-    const [editedQuote, setEditedQuote] = useState({});
-    const [isAlertDialogOpen, setIsAlertDialogOpen] = useState(false);
-    const [quoteToDelete, setQuoteToDelete] = useState(null);
-    const [isSuccessMessageVisible, setIsSuccessMessageVisible] = useState(false);
+  const [quotes, setQuotes] = useState([]);
+  const [selectedQuote, setSelectedQuote] = useState(null);
+  const [selectedColor, setSelectedColor] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedQuote, setEditedQuote] = useState({});
+  const [isAlertDialogOpen, setIsAlertDialogOpen] = useState(false);
+  const [quoteToDelete, setQuoteToDelete] = useState(null);
+  const [isSuccessMessageVisible, setIsSuccessMessageVisible] = useState(false);
 
-    const [currentPage, setCurrentPage] = useState(1); 
-    const quotesPerPage = 10; 
+  const [currentPage, setCurrentPage] = useState(1);
+  const quotesPerPage = 10;
 
-    const fetchQuotes = async () => {
-        try {
-            const response = await fetch('http://127.0.0.1:8000/api/quotes');
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            setQuotes(data);
-        } catch (error) {
-            console.error("Error fetching quotes:", error);
-        }
-    };
+  const fetchQuotes = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/quotes");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setQuotes(data);
+    } catch (error) {
+      console.error("Error fetching quotes:", error);
+    }
+  };
 
-    useEffect(() => {
-        fetchQuotes();
-    }, []);
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
 
-    const indexOfLastQuote = currentPage * quotesPerPage;
-    const indexOfFirstQuote = indexOfLastQuote - quotesPerPage;
-    const currentQuotes = quotes.slice(indexOfFirstQuote, indexOfLastQuote);
+  const indexOfLastQuote = currentPage * quotesPerPage;
+  const indexOfFirstQuote = indexOfLastQuote - quotesPerPage;
+  const currentQuotes = quotes.slice(indexOfFirstQuote, indexOfLastQuote);
 
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-    const formatDate = (mongoDate) => {
-        if (!mongoDate) {
-            return "La date n'est pas disponible";
-        }
-        const date = new Date(mongoDate);
-        if (isNaN(date.getTime())) {
-            return "La date n'est pas disponible";
-        }
-        return date.toLocaleDateString("fr-FR", {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-        });
-    };
+  const formatDate = (mongoDate) => {
+    if (!mongoDate) {
+      return "La date n'est pas disponible";
+    }
+    const date = new Date(mongoDate);
+    if (isNaN(date.getTime())) {
+      return "La date n'est pas disponible";
+    }
+    return date.toLocaleDateString("fr-FR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
 
-    const closeModal = () => {
-        setSelectedQuote(null);
-        setSelectedColor('');
-        setIsEditing(false);
-        setEditedQuote({});
-    };
+  const closeModal = () => {
+    setSelectedQuote(null);
+    setSelectedColor("");
+    setIsEditing(false);
+    setEditedQuote({});
+  };
 
-    const handleDelete = (quoteId) => {
-        setQuoteToDelete(quoteId);
-        setIsAlertDialogOpen(true);
-    };
+  const handleDelete = (quoteId) => {
+    setQuoteToDelete(quoteId);
+    setIsAlertDialogOpen(true);
+  };
 
-    const confirmDelete = async () => {
-        try {
-            await axios.delete(`http://127.0.0.1:8000/api/quotes/${quoteToDelete}`);
-            setQuotes((prevQuotes) => prevQuotes.filter(quote => quote.id !== quoteToDelete));
-            setIsSuccessMessageVisible(true);
-            setTimeout(() => {
-                setIsSuccessMessageVisible(false);
-            }, 3000);
-            closeModal();
-        } catch (error) {
-            console.error("Error al eliminar la cita:", error);
-        } finally {
-            setIsAlertDialogOpen(false);
-            setQuoteToDelete(null);
-        }
-    };
+  const confirmDelete = async () => {
+    try {
+      await axios.delete(`http://127.0.0.1:8000/api/quotes/${quoteToDelete}`);
+      setQuotes((prevQuotes) =>
+        prevQuotes.filter((quote) => quote.id !== quoteToDelete)
+      );
+      setIsSuccessMessageVisible(true);
+      setTimeout(() => {
+        setIsSuccessMessageVisible(false);
+      }, 3000);
+      closeModal();
+    } catch (error) {
+      console.error("Error al eliminar la cita:", error);
+    } finally {
+      setIsAlertDialogOpen(false);
+      setQuoteToDelete(null);
+    }
+  };
 
-    const handleEdit = () => {
-        setIsEditing(true);
-        setEditedQuote(selectedQuote);
-    };
+  const handleEdit = () => {
+    setIsEditing(true);
+    setEditedQuote(selectedQuote);
+  };
 
-    const handleSave = async () => {
-        console.log('Guardando cambios...');
-        try {
-            await axios.put(`http://127.0.0.1:8000/api/quotes/${editedQuote.id}`, editedQuote);
-            setQuotes((prevQuotes) =>
-                prevQuotes.map((quote) =>
-                    quote.id === editedQuote.id ? editedQuote : quote
-                )
-            );
-            setIsEditing(false);
-            closeModal();
-        } catch (error) {
-            console.error("Erreur d'enregistrement de la citation éditée:", error);
-        }
-    };
+  const handleSave = async () => {
+    console.log("Guardando cambios...");
+    try {
+      await axios.put(
+        `http://127.0.0.1:8000/api/quotes/${editedQuote.id}`,
+        editedQuote
+      );
+      setQuotes((prevQuotes) =>
+        prevQuotes.map((quote) =>
+          quote.id === editedQuote.id ? editedQuote : quote
+        )
+      );
+      setIsEditing(false);
+      closeModal();
+    } catch (error) {
+      console.error("Erreur d'enregistrement de la citation éditée:", error);
+    }
+  };
 
-    const colors = [
-        { bgColor: 'bg-darkPink', textColor: 'text-cream' },
-        { bgColor: 'bg-lightPink', textColor: 'text-blue' },
-        { bgColor: 'bg-green', textColor: 'text-lightPink' },
-        { bgColor: 'bg-blue', textColor: 'text-black' },
-    ];
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditedQuote((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-    return (
-        <div className="h-screen flex flex-col">
-            <Navbar />
-            <div className={`flex flex-1 justify-center items-center flex-wrap ${selectedQuote ? 'blur-sm' : ''}`}>
-                {currentQuotes.map((quote, index) => {
-                    const formattedDate = quote.date ? formatDate(quote.date) : "La date n'est pas disponible";
-                    const colorClass = colors[index % colors.length];
+  const colors = [
+    { bgColor: "bg-darkPink", textColor: "text-cream" },
+    { bgColor: "bg-lightPink", textColor: "text-blue" },
+    { bgColor: "bg-green", textColor: "text-lightPink" },
+    { bgColor: "bg-blue", textColor: "text-black" },
+  ];
 
-                    return (
-                        <Card
-                            key={quote.id}
-                            className={`relative w-64 h-64 m-4 cursor-pointer hover:shadow-lg ${colorClass.bgColor}`}
-                            onClick={() => {
-                                setSelectedQuote(quote);
-                                setSelectedColor(colorClass.bgColor);
-                            }}
-                        >
-                            <CardContent className={`text-center p-4 flex justify-center items-center h-full ${colorClass.textColor} font-lifeSavers card-content`}>
-                                <p className="text-xl line-clamp-3">{quote.phrase || quote.text}</p>
-                            </CardContent>
-                        </Card>
-                    );
-                })}
-            </div>
-      
-            <Pagination
-                quotesPerPage={quotesPerPage}
-                totalQuotes={quotes.length}
-                paginate={paginate}
-                currentPage={currentPage}
+  return (
+    <div className="h-screen flex flex-col">
+      <Navbar />
+      <div
+        className={`flex flex-1 justify-center items-center flex-wrap ${
+          selectedQuote ? "blur-sm" : ""
+        }`}
+      >
+        {currentQuotes.map((quote, index) => {
+          const formattedDate = quote.date
+            ? formatDate(quote.date)
+            : "La date n'est pas disponible";
+          const colorClass = colors[index % colors.length];
+
+          return (
+            <Card
+              key={quote.id}
+              className={`relative w-64 h-64 m-4 cursor-pointer hover:shadow-lg ${colorClass.bgColor}`}
+              onClick={() => {
+                setSelectedQuote(quote);
+                setSelectedColor(colorClass.bgColor);
+              }}
+            >
+              <CardContent
+                className={`text-center p-4 flex justify-center items-center h-full ${colorClass.textColor} font-lifeSavers card-content`}
+              >
+                <p className="text-xl line-clamp-3">
+                  {quote.phrase || quote.text}
+                </p>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </div>
+
+      <Pagination
+        quotesPerPage={quotesPerPage}
+        totalQuotes={quotes.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
+
+      {selectedQuote && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50">
+          <div
+            className={`p-6 rounded-lg shadow-lg max-w-xl w-full relative z-50 ${selectedColor}`}
+          >
+            <IoCloseOutline
+              className="absolute top-4 right-4 text-gray-500 cursor-pointer"
+              size={24}
+              onClick={closeModal}
             />
-
-            {selectedQuote && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50">
-                    <div className={`p-6 rounded-lg shadow-lg max-w-xl w-full relative z-50 ${selectedColor}`}>
-                        <IoCloseOutline
-                            className="absolute top-4 right-4 text-gray-500 cursor-pointer"
-                            size={24}
-                            onClick={closeModal}
-                        />
-                        {isEditing ? (
-                            <div className="p-4 font-lifeSavers">
-                            </div>
-                        ) : (
-                            <CardContent className="p-4 overflow-y-auto max-h-96 font-lifeSavers">
-                                <p className="text-lg mb-4">“{selectedQuote.phrase || selectedQuote.text}”</p>
-                                <div className="mt-4 text-sm text-gray-600">
-                                    <p><strong>Livre:</strong> {selectedQuote.title || "Le titre n'est pas disponible"}</p>
-                                    <p><strong>Auteur:</strong> {selectedQuote.author || "L'auteur n'est pas disponible"}</p>
-                                    <p><strong>Date:</strong> {formatDate(selectedQuote.date)}</p>
-                                </div>
-                            </CardContent>
-                        )}
-
-                        <CardFooter className="flex justify-end space-x-2 p-4">
-                            {!isEditing && (
-                                <button
-                                    className="flex items-center space-x-2 px-4 py-2 bg-blue-500 text-white rounded"
-                                    onClick={handleEdit}
-                                >
-                                    <FaPenNib />
-                                    <span className="font-lifeSavers">Éditer</span>
-                                </button>
-                            )}
-                            {!isEditing && (
-                                <button
-                                    className="flex items-center space-x-2 px-4 py-2 bg-red-500 text-white rounded"
-                                    onClick={() => handleDelete(selectedQuote.id)}
-                                >
-                                    <FaTrash />
-                                    <span className="font-lifeSavers">Éliminer</span>
-                                </button>
-                            )}
-                        </CardFooter>
-                    </div>
+            {isEditing ? (
+              <div className="p-4 font-lifeSavers">
+                <h2 className="text-xl mb-4">Éditez la citation</h2>
+                <div className="space-y-4">
+                  <input
+                    type="text"
+                    name="phrase"
+                    value={editedQuote.phrase || ""}
+                    onChange={handleChange}
+                    placeholder="Phrase"
+                    className="w-full p-2 border border-gray-300 rounded"
+                  />
+                  <input
+                    type="text"
+                    name="title"
+                    value={editedQuote.title || ""}
+                    onChange={handleChange}
+                    placeholder="Livre"
+                    className="w-full p-2 border border-gray-300 rounded"
+                  />
+                  <input
+                    type="text"
+                    name="author"
+                    value={editedQuote.author || ""}
+                    onChange={handleChange}
+                    placeholder="Auteur"
+                    className="w-full p-2 border border-gray-300 rounded"
+                  />
+                  <div className="flex justify-center">
+                    <button
+                      onClick={handleSave}
+                      className="px-4 py-2 bg-green-500 text-white rounded flex items-center space-x-2"
+                    >
+                      <FaSave />
+                      <span>Enregistrer</span>
+                    </button>
+                  </div>
                 </div>
+              </div>
+            ) : (
+              <CardContent className="p-4 overflow-y-auto max-h-96 font-lifeSavers">
+                <p className="text-lg mb-4">
+                  “{selectedQuote.phrase || selectedQuote.text}”
+                </p>
+                <div className="mt-4 text-sm text-gray-600">
+                  <p>
+                    <strong>Livre:</strong>{" "}
+                    {selectedQuote.title || "Le titre n'est pas disponible"}
+                  </p>
+                  <p>
+                    <strong>Auteur:</strong>{" "}
+                    {selectedQuote.author || "L'auteur n'est pas disponible"}
+                  </p>
+                  <p>
+                    <strong>Date:</strong> {formatDate(selectedQuote.date)}
+                  </p>
+                </div>
+              </CardContent>
             )}
 
-            {isAlertDialogOpen && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50 font-lifeSavers">
-                    <div className="p-6 rounded-lg shadow-lg bg-cream">
-                        <h2 className="text-xl font-bold mb-4">Confirmation</h2>
-                        <p>Êtes-vous sûr de vouloir supprimer cette citation ?</p>
-                        <div className="flex justify-end mt-4">
-                            <button
-                                className="px-4 py-2 bg-red-500 text-white rounded font-bold"
-                                onClick={confirmDelete}
-                            >
-                                Oui, supprimer
-                            </button>
-                            <button
-                                className="px-4 py-2 bg-gray-300 rounded ml-2 font-bold"
-                                onClick={() => setIsAlertDialogOpen(false)}
-                            >
-                                Annuler
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            )}
+            <CardFooter className="flex justify-end space-x-2 p-4">
+              {!isEditing && (
+                <button
+                  className="flex items-center space-x-2 px-4 py-2 bg-blue-500 text-white rounded"
+                  onClick={handleEdit}
+                >
+                  <FaPenNib />
+                  <span className="font-lifeSavers">Éditer</span>
+                </button>
+              )}
+              {!isEditing && (
+                <button
+                  className="flex items-center space-x-2 px-4 py-2 bg-red-500 text-white rounded"
+                  onClick={() => handleDelete(selectedQuote.id)}
+                >
+                  <FaTrash />
+                  <span className="font-lifeSavers">Éliminer</span>
+                </button>
+              )}
+            </CardFooter>
+          </div>
         </div>
-    );
+      )}
+
+      {/* Dialog de confirmación de eliminación */}
+      {isAlertDialogOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50 font-lifeSavers">
+          <div className="p-6 rounded-lg shadow-lg bg-cream">
+            <h2 className="text-xl mb-4">
+              Êtes-vous sûr de vouloir supprimer cette citation ?
+            </h2>
+            <div className="flex justify-end space-x-2">
+              <button
+                className="px-4 py-2 bg-gray-500 text-white rounded"
+                onClick={() => setIsAlertDialogOpen(false)}
+              >
+                Annuler
+              </button>
+              <button
+                className="px-4 py-2 bg-red-500 text-white rounded"
+                onClick={confirmDelete}
+              >
+                Supprimer
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Mensaje de éxito */}
+      {isSuccessMessageVisible && (
+        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-green-500 text-white p-4 rounded-lg shadow-lg z-50">
+          <p className="font-lifeSavers">Citation supprimée avec succès!</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default QuotesView;
-
