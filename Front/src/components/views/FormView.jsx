0c91f862d89fd74967c47ae3e462721239930664
Claudia González <claudia.glezg@gmail.com>
2024-10-19 14:03:46 +0200

    Change the border round and elevate the title in the formview

--- Front/src/components/views/FormView.jsx
+++ Front/src/components/views/FormView.jsx
@@ -13,8 +13,8 @@ const FormView = () => {
     <PageLayout breadcrumbItems={breadcrumbItems}>
       <div className='flex items-center justify-center h-screen'>
         <div className='flex flex-col items-center w-full px-4'>
-          <h1 className='font-lifeSavers text-darkPink p-4 text-3xl'>Ajouter une phrase</h1>
-          <div className='bg-darkPink p-7 w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl mx-auto'>
+          <h1 className='font-lifeSavers text-darkPink p-4 text-3xl font-bold mt-[-60px] mb-6'>Ajouter une phrase</h1>
+          <div className='bg-darkPink p-7 w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl mx-auto rounded-lg'>
             <ProfileForm />
           </div>
         </div>
